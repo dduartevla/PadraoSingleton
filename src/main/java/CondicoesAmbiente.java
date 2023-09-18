1ac8b1de35d579512c39fe8f6701312f917a29d8
dduartevla <deboraduarte@ice.ufjf.br>
2023-09-17 21:24:26 -0300

    Termina a Classe CondicoesAmbiente

--- src/main/java/CondicoesAmbiente.java
+++ src/main/java/CondicoesAmbiente.java
@@ -2,6 +2,61 @@ public class CondicoesAmbiente {
 
     private CondicoesAmbiente() {}
 
+    private static CondicoesAmbiente instance = new CondicoesAmbiente();
 
+    public static CondicoesAmbiente getInstance() {
+        return instance;
+    }
 
+    private int temperaturaIdeal;
+    private int umidadeIdeal;
+    private int luminosidadeIdeal;
+
+    public boolean isTemperaturaOk(int temperaturaLida){
+        boolean isOk = true;
+        if(this.temperaturaIdeal != temperaturaLida){
+            isOk = false;
+        }
+        return isOk;
+    }
+
+    public boolean isUmidadeOk(int umidadeLida){
+        boolean isOk = true;
+        if(this.umidadeIdeal != umidadeLida){
+            isOk = false;
+        }
+        return isOk;
+    }
+
+    public boolean isLuminosidadeOk(int luminosidadeLida){
+        boolean isOk = true;
+        if(this.luminosidadeIdeal != luminosidadeLida){
+            isOk = false;
+        }
+        return isOk;
+    }
+
+    public int getTemperaturaIdeal() {
+        return temperaturaIdeal;
+    }
+
+    public void setTemperaturaIdeal(int temperaturaIdeal) {
+        this.temperaturaIdeal = temperaturaIdeal;
+    }
+
+    public int getUmidadeIdeal() {
+        return umidadeIdeal;
+    }
+
+    public void setUmidadeIdeal(int umidadeIdeal) {
+        this.umidadeIdeal = umidadeIdeal;
+    }
+
+    public int getLuminosidadeIdeal() {
+        return luminosidadeIdeal;
+    }
+
+    public void setLuminosidadeIdeal(int luminosidadeIdeal) {
+        this.luminosidadeIdeal = luminosidadeIdeal;
+    }
 }
